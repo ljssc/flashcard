9c4d997d0468bffbfa1457fd1e5895b3447ebe26
ljssc <igor.salnikov.00@bk.ru>
2025-10-01 10:30:29 +0500

    new card

---
+++ src/data.js
@@ -0,0 +1,64 @@
+import img1 from './img/pumpkin.jpg'
+import img2 from './img/apple.jpg'
+import img3 from './img/coconut.jpg'
+import img4 from './img/orange.jpg'
+import img5 from './img/pomegranate.jpg'
+import img6 from './img/avocado.jpg'
+
+
+const images = [
+    {
+       "id":1,
+       "url":img1
+    },
+    {
+       "id":2,
+       "url":img2
+    },
+    {
+       "id":3,
+       "url":img3
+    },
+    {
+       "id":4,
+       "url":img4
+    },
+    {
+       "id":5,
+       "url":img5
+    },
+    {
+       "id":6,
+       "url":img6
+    },
+    {
+       "id":7,
+       "url":img1
+    },
+    {
+       "id":8,
+       "url":img2
+    },
+    {
+       "id":9,
+       "url":img3
+    },
+    {
+       "id":10,
+       "url":img4
+    },
+    {
+       "id":11,
+       "url":img5
+    },
+    {
+       "id":12,
+       "url":img6
+    }
+    
+
+    
+];
+
+
+export {images}
